83afdb413f408705c50849f64023a930de670da8
hilmaadiafadila <hilmaadiafadila2007@gmail.com>
2026-09-22 19:56:16 +0700

    edit link

--- script.js
+++ script.js
@@ -10,13 +10,13 @@ const projects = [
         title: "Slicing Website Portfolio",
         tech: "HTML, CSS, JavaScript",
         desc: "Tugas kuliah slicing website responsive dengan DOM manipulation.",
-        github: "https://github.com/razshelia/CollabBuy.git"
+        // github: "https://github.com/razshelia/CollabBuy.git"
     },
     {
         title: "RakitKru.id",
         tech: "Business Development, Pitch Deck",
         desc: "Project kompetisi GEMASTIK XIX — platform fractional talent cloud.",
-        github: "https://github.com/hilmaadiafadila-collab/SewaAlatCamping.git"
+        // github: "https://github.com/hilmaadiafadila-collab/SewaAlatCamping.git"
     }
 ];
 
